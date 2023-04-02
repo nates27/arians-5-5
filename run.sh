539aacc2f29b1fb122f69601265fdb1f3ce44ec4
nates27 <nates.singko@gmail.com>
2023-04-02 16:44:28 +0800

    modified:   run.sh new file:   tradestrat_final.py new file:   tradestrat_v3_oldf.py

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # $2:the absolute path of output file
 
 if [[ -n "$1" && -n "$2" ]]; then
-    python tradestrat_v1.py $1 $2
+    python tradestrat_final.py $1 $2
 else
     echo "Insufficient input parameters!"
 fi
